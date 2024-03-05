23c2d20c0154fc0bcac34127a42af4691a9a2d02
Higor Silva <48228780+higorslva@users.noreply.github.com>
2024-03-05 13:28:43 -0300

    Tratado o erro ao receber frames vazios
    Um erro ocorria caso um pacote fosse perdido. Agora um tratamento é feito para, caso o pacote seja igual a zero, ele apenas é ignorado.

--- main_camera.py
+++ main_camera.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import socket
 import time
@@ -12,7 +11,7 @@ with open("coco.names", "r") as f:
 # Configurações do servidor UDP
 host = '0.0.0.0'  # Escuta em todos os endereços disponíveis
 port = 12345  # Porta UDP
-buffer_size = 65507  # Tamanho máximo de um pacote UDP
+buffer_size = 999999999  # Tamanho máximo de um pacote UDP
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((host, port))
@@ -23,33 +22,28 @@ model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(416, 416), scale=1/255)
 
 while True:
-
     data, _ = sock.recvfrom(65535)  # Recebe os dados do endereço da Raspberry Pi
     frame = np.frombuffer(data, dtype=np.uint8)  # Converte os dados recebidos de volta para o frame
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)  # Decodifica o frame JPEG
+    
+    if frame is not None:  # Verifica se o frame não é vazio
+        start = time.time()
 
-    start = time.time()
+        classes, scores, boxes = model.detect(frame, 0.1, 0.2)
 
-    classes, scores, boxes = model.detect(frame, 0.1, 0.2)
+        end = time.time()
 
-    end = time.time()
+        for (classid, score, box) in zip(classes, scores, boxes):
+            color = COLORS[int(classid) % len(COLORS)]
+            label = f'{class_names[classid]} : {score*100:.2f}%'
+            cv2.rectangle(frame, box, color, 2)
+            cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-    for (classid, score, box) in zip(classes, scores, boxes):
+        fps_label = f'FPS: {round((1.0 / (end - start)), 2)}'
+        cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
+        cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
-        color = COLORS[int(classid) % len(COLORS)]
-
-        label = f'{class_names[classid]} : {score*100:.2f}%'
-
-        cv2.rectangle(frame, box, color, 2)
-
-        cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-    fps_label = f'FPS: {round((1.0 / (end - start)), 2)}'
-
-    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
-    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-
-    cv2.imshow("detections", frame)
+        cv2.imshow("detections", frame)
 
     if cv2.waitKey(1) == 27:
         break
