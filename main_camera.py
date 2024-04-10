a9e42db62ea4260b2cab16cb7a747bbf5d055a9d
Higor Silva <higor.slva@outlook.com>
2024-04-10 12:10:51 -0300

    detecção de placas de carros

--- main_camera.py
+++ main_camera.py
@@ -2,11 +2,12 @@ import cv2
 import numpy as np
 import socket
 import time
+import logging
 
 COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
 class_names = []
-with open("coco.names", "r") as f:
+with open("plates.names", "r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
 
 # Configurações do servidor UDP
@@ -17,7 +18,7 @@ buffer_size = 65507  # Tamanho máximo de um pacote UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((host, port))
 
-net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4-tiny.cfg")
+net = cv2.dnn.readNet("plates-yolov4-tiny-detector.weights", "plates-yolov4-tiny-detector.cfg")
 
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(416, 416), scale=1/255)
@@ -26,25 +27,33 @@ while True:
     data, _ = sock.recvfrom(65535)  # Recebe os dados do endereço da Raspberry Pi
     frame = np.frombuffer(data, dtype=np.uint8)  # Converte os dados recebidos de volta para o frame
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)  # Decodifica o frame JPEG
-    
-    if frame is not None:  # Verifica se o frame não é vazio
-        start = time.time()
 
-        classes, scores, boxes = model.detect(frame, 0.1, 0.2)
+    if frame is None or frame.size == 0:
+        logging.error("Frame vazio ou inválido.")
+        continue
 
-        end = time.time()
+    # Redimensionar o frame para as dimensões corretas
+    frame = cv2.resize(frame, (416, 416))
 
-        for (classid, score, box) in zip(classes, scores, boxes):
-            color = COLORS[int(classid) % len(COLORS)]
-            label = f'{class_names[classid]} : {score*100:.2f}%'
-            cv2.rectangle(frame, box, color, 2)
-            cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    start = time.time()
 
-        fps_label = f'FPS: {round((1.0 / (end - start)), 2)}'
-        cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
-        cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+    classes, scores, boxes = model.detect(frame, confThreshold=0.1, nmsThreshold=0.2)
 
-        cv2.imshow("detections", frame)
+    end = time.time()
+
+    for (classid, score, box) in zip(classes, scores, boxes):
+        class_id = classid[0] if isinstance(classid, np.ndarray) else classid
+        score_val = score[0] if isinstance(score, np.ndarray) else score
+        label = f'{class_names[int(class_id)]} : {round(float(score_val), 2)}'
+        color = COLORS[int(class_id) % len(COLORS)]
+        cv2.rectangle(frame, box, color, 2)
+        cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    fps_label = f'FPS: {round((1.0 / (end - start)), 2)}'
+    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
+    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+    cv2.imshow("detections", frame)
 
     if cv2.waitKey(1) == 27:
         break
